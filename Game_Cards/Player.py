98839773ec7e53e4cc0dfb7ebfe404f9108bbf1d
Raz <Razbk123@gmail.com>
2024-12-16 12:27:36 +0200

    Player and test_Player edits in class

--- Game_Cards/Player.py
+++ Game_Cards/Player.py
@@ -9,7 +9,7 @@ class Player:
         if type(name) != str:
             raise TypeError("name must be a string!")
         self.name = name
-        if 10 <= cards_number <= 26:
+        if 10 <= cards_number <= 26: # Checks for the cards number to be within the allowed range.
             self.cards_number = cards_number
         else:
             self.cards_number = 26
@@ -19,32 +19,19 @@ class Player:
         return f"-----[{self.name} - {len(self.player_cards)} Cards]-----\n{self.player_cards}"
 
     def set_hand(self, cards_deck = DeckOfCards()):
+        """Gives the player a deck of cards according to the cards_number"""
         if type(cards_deck) != DeckOfCards:
             raise TypeError("Cards Deck must be a DeckOfCards Type!")
-        if len(cards_deck.cards) > 0:
+        if len(cards_deck.cards) > 0: # if there are cards in the cards deck
             for cards in range(self.cards_number):
-                self.player_cards.append(cards_deck.deal_one())
+                self.player_cards.append(cards_deck.deal_one()) #adds a card from the cards deck to the player
 
     def get_card(self):
+        """The method takes one random card from the player"""
         if len(self.player_cards) > 0:
             return self.player_cards.pop(randint(0, len(self.player_cards) - 1))
 
     def add_card(self, new_card):
+        """Method that adds a card to the user"""
         if type(new_card) == Card:
             self.player_cards.append(new_card)
-
-if __name__ == "__main__":
-    player1 = Player("Raz", 10)
-    game_cards = DeckOfCards()
-    print(game_cards)
-    player1.set_hand(game_cards)
-    print(game_cards)
-    print(player1.player_cards)
-    print("GAME CARDS")
-    print(player1.get_card())
-    print(player1.get_card())
-    print(player1.get_card())
-    print(player1.player_cards)
-    player1.add_card(game_cards.deal_one())
-    print(player1.player_cards)
-    print(game_cards)
